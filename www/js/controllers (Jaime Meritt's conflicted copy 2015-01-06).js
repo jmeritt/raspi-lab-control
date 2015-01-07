04e761247e0b3655407444bf429aa5bd96ebab4e
jmeritt <Jaime.Meritt@gmail.com>
2015-01-06 20:25:01 -0500

    Initial checkin

---
+++ www/js/controllers (Jaime Meritt's conflicted copy 2015-01-06).js
@@ -0,0 +1,34 @@
+angular.module('starter.controllers', [])
+
+.controller('TestsCtrl', function($scope, Tests) {
+  $scope.tests = Tests.all();
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('TestDetailCtrl', function($scope, $stateParams, Tests){
+  $scope.test = Tests.get($stateParams.testId);
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
